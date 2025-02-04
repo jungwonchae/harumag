c3cbcf2de7cc823c937f9ba90b698f436eb0013f
Jiyun Woo <a01052919770@gmail.com>
2025-02-05 01:38:37 +0900

    Initial commit

---
+++ src/main/java/com/example/harumag/HarumagApplication.java
@@ -0,0 +1,13 @@
+package com.example.harumag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarumagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarumagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/harumag/HarumagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harumag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarumagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
